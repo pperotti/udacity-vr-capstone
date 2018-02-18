8265542dee9a4fbbf2950d1c6b50de13195e2635
Pablo Perotti <pablo@blackpixel.com>
2018-02-18 03:02:00 -0300

    feature: Add numeric keyboard, score and dialog panel.

---
+++ Assets/NumericKeyboard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumericKeyboard : MonoBehaviour {
+
+	//Text
+	public UnityEngine.UI.Text ipText;
+
+	public void onButtonSelected(string value) {
+		Debug.Log ("onButtonSelected: 1" + ipText);
+		if (ipText != null)
+		{			
+			ipText.text += value;
+		}
+	}
+
+	public void onBackPressed() {
+		Debug.Log ("onBackPressed");
+		if (ipText != null)
+		{
+			if (ipText.text.Length == 1) {
+				ipText.text = "";
+			} 
+			else if (ipText.text.Length > 1) 
+			{
+				ipText.text = ipText.text.Substring(0, ipText.text.Length - 1);	
+			}
+		}
+	}
+}
