8759fe285fa31399d9dc36d3c59037b428c56afe
Pablo Perotti <pablo@blackpixel.com>
2018-02-23 04:13:18 -0300

    feature: Add welcome screen.

---
+++ Assets/Scripts/QuitBehavior.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitBehavior : MonoBehaviour {
+
+	public void Exit() {
+		Application.Quit ();
+	}
+}
