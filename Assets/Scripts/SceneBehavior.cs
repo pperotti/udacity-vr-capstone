c7fca3203883edbc618aaf7a3b1581834aec9e63
Pablo Perotti <pablo@blackpixel.com>
2018-02-23 03:39:39 -0300

    feature: Add credits scene functionality.

---
+++ Assets/Scripts/SceneBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneBehavior : MonoBehaviour {
+
+	//Method to change to a different scene.
+	public void ChangeToScene(string sceneName) {
+		if (sceneName != null && sceneName.Length > 0) {
+			SceneManager.LoadScene (sceneName);
+		}
+	}
+}
